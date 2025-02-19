b68881a842da83bc8cab77637e7add85e9a012b5
Wazid <munavalliwazid@gmail.com>
2025-02-19 13:15:11 +0530

    requiring single function

---
+++ exports&require/math.js
@@ -0,0 +1,15 @@
+
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+Module.exports={add,subtract}
+
+// module.exports = add; // Exporting single  function
+
+
