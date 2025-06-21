855efd9fb2df02da418250c93fe51c883ad8c19a
Wazid <munavalliwazid@gmail.com>
2025-06-21 18:59:21 +0530

    schema validation

--- backend/src/models/user.js
+++ backend/src/models/user.js
@@ -2,16 +2,43 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
     firstName:{
-        type:"string"
+        type:String,
+        required:true,
+        minlength:4,
+        maxlength:12
     },
     lastName:{
-        type:"String"
+        type:String
+    },
+    gender:{
+        type:String,
+        validate:{
+            validator:function(value){
+                if(value !== 'male' && value !== 'female' && value !== 'other'){
+                    return false;
+                }
+                return true;
+            },
+            message:props=> `${props.value} is not valid gender`
+        }
     },
     email:{
-        type:"string"
+        type:String,
+        unique:true,
+        required:true
     },
     password:{
-        type:"string"
+        type:String
+    },
+    Bio:{
+        type:String
+    },
+    skills:{
+        type:[String]
+    },
+    profilePicURL:{
+        type:"String",
+        default:"https://static.vecteezy.com/system/resources/thumbnails/020/911/740/small_2x/user-profile-icon-profile-avatar-user-icon-male-icon-face-icon-profile-icon-free-png.png"
     }
 });
 
