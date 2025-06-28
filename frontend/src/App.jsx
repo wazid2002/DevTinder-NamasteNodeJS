6e24921d4314809ee8eee8ed1e106c2938e3d6ba
Wazid <munavalliwazid@gmail.com>
2025-06-28 16:49:20 +0530

    feat: add Routing & child routes like login and profile for parent Body

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,21 @@
-import NavBar from "./NavBar";
+import Body from "./Body";
+import Login from "./Login";
+import Profile from "./profile"
+import { BrowserRouter,Route,Routes } from "react-router-dom";
 
 export default function App() {
   return (
-    <NavBar />
-  )
+
+    <BrowserRouter basename="/">
+      <Routes>
+        <Route path="/" element={<Body/>}>
+        <Route path="login" element={<Login/>}/>
+        <Route path="profile" element={<Profile/>}/>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+
+  );
 }
+
+
---
+++ frontend/src/Body.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import NavBar from './NavBar'
+import { Outlet } from 'react-router-dom'
+import Footer from './Footer'
+
+const Body = () => {
+  return (
+    <div>
+        <NavBar />
+        <Outlet />
+        <Footer/>
+    </div>
+    
+  )
+}
+
+export default Body
